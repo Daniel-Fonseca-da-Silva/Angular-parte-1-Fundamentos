d6324c9848cf0eaadcfbcdc18b0616e4ae546db7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-12 04:08:24 -0300

    Criando o primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alurapic';
-  description = "Tanque";
-  url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Hetzer_lesany.jpg";
+    
 }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+  description = "Tanque";
+  url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Hetzer_lesany.jpg";
+}

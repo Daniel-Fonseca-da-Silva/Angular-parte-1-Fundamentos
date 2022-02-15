fb7d823db6bd75804786e0163c16543b111e5979
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-15 04:04:50 -0300

    Ciclo de vida de um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
@@ -6,11 +6,14 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   photos: Object[] = [];
 
-  constructor(photoService: PhotoService) {
-    photoService.listFromUser('flavio')
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio')
     .subscribe(photos => this.photos = photos);
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    PhotosModule,
-    HttpClientModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]

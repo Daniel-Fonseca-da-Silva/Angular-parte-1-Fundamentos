20530cbdc0a0fb7d8d50cd3fb7ade46727861f35
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-16 00:03:14 -0300

    Single Page Application e Rotas

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule{}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
-
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 
 @NgModule({
   declarations: [ 
     PhotoComponent, 
-    PhotoListComponent ],
+    PhotoListComponent, 
+    PhotoFormComponent 
+  ],
   imports: [ 
     HttpClientModule,
     CommonModule

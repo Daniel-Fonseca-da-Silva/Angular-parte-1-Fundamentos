40a0d8b99784c3cb35c673495d40f0577d8e1145
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-15 22:14:28 -0300

    Organizando ainda mais nossa aplicação

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Photo } from "./photo";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
 

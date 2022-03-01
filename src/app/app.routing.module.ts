28d710420bd157efebd05a1fd3921e772845ca15
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-01 13:55:24 -0300

    Resolvers

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,11 +4,24 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent },
-  { path: 'p/add', component: PhotoFormComponent },
-  { path: '**', component: NotFoundComponent }
+  { 
+    path: 'user/:userName', 
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver
+    }
+  },
+  { 
+    path: 'p/add',
+     component: PhotoFormComponent 
+  },
+  { 
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({

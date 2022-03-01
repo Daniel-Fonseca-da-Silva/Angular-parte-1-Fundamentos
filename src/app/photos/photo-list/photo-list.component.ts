b96370e83b7f780d13a73a380fafbdb0ebf52310
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-01 00:49:56 -0300

    Pipe e implementação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
 
   constructor(
     private photoService: PhotoService,
